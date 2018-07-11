66ad942a20fe7bcdd4921a442beba57b63f9cbd8
riverDeer12 <riverdeer95@gmail.com>
2018-07-11 14:32:09 +0200

    - Changed logo of application and splash screen

--- src/pages/closest-location/closest-location.ts
+++ src/pages/closest-location/closest-location.ts
@@ -21,38 +21,22 @@ export class ClosestLocationPage {
   public locationImgSource;
   public tourImgSource;
 
-  constructor(
-    public navCtrl: NavController,
-    public navParams: NavParams,
-    public geoLocation: Geolocation,
-    public httpClient: HttpClient,
-    private alertCtrl: AlertController,
-    public splashScreen: SplashScreen
-  ) {
+  constructor(public navCtrl: NavController,public navParams: NavParams,public geoLocation: Geolocation,public httpClient: HttpClient, private alertCtrl: AlertController,public splashScreen: SplashScreen) {
     this.splashScreen.show();
-    this.geoLocation
-      .getCurrentPosition()
+    this.geoLocation.getCurrentPosition()
       .then(resp => {
         this.myLatitude = resp.coords.latitude;
         this.myLongitude = resp.coords.longitude;
-        this.closest = this.httpClient.get(
-          "http://krk-api.herokuapp.com/" +
-            this.myLatitude +
-            "," +
-            this.myLongitude
-        );
+        this.closest = this.httpClient.get("http://krk-api.herokuapp.com/" + this.myLatitude + "," + this.myLongitude);
         this.closest.subscribe(data => {
           this.closestLocation = data["Closest location"]["name"]["eng"];
           this.closestTour = data["Closest tour"]["name"]["eng"];
           this.locationType = data["Closest location"]["type"];
-          this.locationImgSource =
-            "assets/imgs/" + data["Closest location"]["key2"] + ".jpg";
-          this.tourImgSource =
-            "assets/imgs/" + data["Closest tour"]["key2"] + ".jpg";
+          this.locationImgSource = "assets/imgs/" + data["Closest location"]["key2"] + ".jpg";
+          this.tourImgSource = "assets/imgs/" + data["Closest tour"]["key2"] + ".jpg";
           this.splashScreen.hide();
         });
-      })
-      .catch(error => {
+      }).catch(error => {
         let alert = this.alertCtrl.create({
           title: "Error getting your location",
           message: "Please check your location settings.",
--- src/pages/tour-details/tour-details.ts
+++ src/pages/tour-details/tour-details.ts
@@ -24,13 +24,30 @@ export class TourDetailsPage {
   }
 
   show_tour(){
-    let tour_links = [];
-    tour_links[0] = 'https://www.google.com/maps/d/u/0/viewer?hl=en&hl=en&mid=1fqyKxf602KThsP8_xm936VhcBFhZduyS&ll=45.04913144399149%2C14.669200000000046&z=11';
-    tour_links[1] = '';
-    tour_links[2] = '';
-    tour_links[3] = '';
-    tour_links[4] = '';
-    this.iab.create(tour_links[this.tourId]);
+    let tour_title = document.getElementById('tourTitleHeader').textContent;
+    
+    if(tour_title == 'Obrambeni zidovi otoka Krka'){
+      this.iab.create('https://www.google.com/maps/d/u/0/viewer?mid=1fqyKxf602KThsP8_xm936VhcBFhZduyS&ll=45.08888834670806%2C14.728269447460889&z=11');
+    }else if(tour_title == 'Defensive walls of the island of Krk'){
+      this.iab.create('https://www.google.com/maps/d/u/0/viewer?mid=1wBPSY8KB7_mYPb6R9AZAPpOZ4Q3987Ey&ll=45.04913144399149%2C14.669200000000046&z=11');
+    }else if(tour_title == 'Posjet samostanima otoka Krka'){
+      this.iab.create('https://www.google.com/maps/d/u/0/viewer?hl=en&mid=1biyXvz6VxSmz9iO8v2vn-6WWTT6Wetb0&ll=45.07197973143436%2C14.535032999999999&z=12');
+    }else if(tour_title == 'A visit to the monasteries of the island of Krk'){
+      this.iab.create('https://www.google.com/maps/d/u/0/viewer?mid=1yGYv114C8evJKzk8kR94ogjrilmpcxEp&ll=45.07197973143436%2C14.535032999999999&z=12');
+    }else if(tour_title == 'Otočka prošlost u tek nekoliko kamenih ostataka'){
+      this.iab.create('https://www.google.com/maps/d/u/0/viewer?mid=1dAKznUtrcyWHxK4H8ssXie0zNSBTILqP&ll=45.098084903041304%2C14.605628499999966&z=11');
+    }else if(tour_title == 'The past of the island preserved in a few stone remains'){
+      this.iab.create('https://www.google.com/maps/d/u/0/viewer?mid=1nF-hsfF4PTIljy_9jn7cdM5Hz7pFx-CW&ll=45.098084903041304%2C14.605628499999966&z=11');
+    }else if(tour_title == 'Male i skrivene crkve otoka Krka'){
+      this.iab.create('https://www.google.com/maps/d/u/0/viewer?mid=1UPulMg7qOXwBQbhqwOiotGv9OHs2wnna&ll=45.11137389050382%2C14.642119999999977&z=12');
+    }else if(tour_title == 'Small and secluded churches of the island of Krk'){
+      this.iab.create('https://www.google.com/maps/d/u/0/viewer?mid=1K7qvA2Ilkz6XM3lMl8sGDHp0K_iFkDPT&ll=45.11137389050382%2C14.642119999999977&z=12');
+    }else if(tour_title == 'Otok Krk u prvim stoljećima nove ere'){
+      this.iab.create('https://www.google.com/maps/d/u/0/viewer?mid=1DGkTztSJTysZAydIsQIMa5ZMxFHJW_8g&ll=45.08839558346452%2C14.643850499999985&z=11');
+    }else if(tour_title == 'The island of Krk in the first centuries of the new age'){
+      this.iab.create('https://www.google.com/maps/d/u/0/viewer?mid=1BmErP9jUAvUdG9yaljOrlLUbSXC4XKjx&ll=45.08839558346452%2C14.643850499999985&z=11');
+    }
+        
   }
 
 }
