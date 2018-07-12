7bbb3ce08eba779b994d1247148c1ccf3efe3d17
riverDeer12 <riverdeer95@gmail.com>
2018-07-12 16:13:30 +0200

    - Fixing closest-location funcionality on ios

--- src/pages/closest-location/closest-location.ts
+++ src/pages/closest-location/closest-location.ts
@@ -1,10 +1,9 @@
-import { Component } from "@angular/core";
-import { IonicPage, NavController, NavParams } from "ionic-angular";
-import { Geolocation } from "@ionic-native/geolocation";
-import { HttpClient } from "@angular/common/http";
-import { Observable } from "rxjs/Observable";
-import { AlertController } from "ionic-angular";
-import { SplashScreen } from "@ionic-native/splash-screen";
+import {Component} from "@angular/core";
+import {IonicPage, NavController, NavParams} from "ionic-angular";
+import {Geolocation} from "@ionic-native/geolocation";
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs/Observable";
+import {LoadingController} from "ionic-angular";
 
 @IonicPage()
 @Component({
@@ -21,34 +20,45 @@ export class ClosestLocationPage {
   public locationImgSource;
   public tourImgSource;
 
-  constructor(public navCtrl: NavController,public navParams: NavParams,public geoLocation: Geolocation,public httpClient: HttpClient, private alertCtrl: AlertController,public splashScreen: SplashScreen) {
-    this.splashScreen.show();
-    this.geoLocation.getCurrentPosition()
-      .then(resp => {
-        this.myLatitude = resp.coords.latitude;
-        this.myLongitude = resp.coords.longitude;
-        this.closest = this.httpClient.get("http://krk-api.herokuapp.com/" + this.myLatitude + "," + this.myLongitude);
-        this.closest.subscribe(data => {
-          this.closestLocation = data["Closest location"]["name"]["eng"];
-          this.closestTour = data["Closest tour"]["name"]["eng"];
-          this.locationType = data["Closest location"]["type"];
-          this.locationImgSource = "assets/imgs/" + data["Closest location"]["key2"] + ".jpg";
-          this.tourImgSource = "assets/imgs/" + data["Closest tour"]["key2"] + ".jpg";
-          this.splashScreen.hide();
-        });
-      }).catch(error => {
-        let alert = this.alertCtrl.create({
-          title: "Error getting your location",
-          message: "Please check your location settings.",
-          buttons: [
-            {
-              text: "Ok",
-              role: "cancel"
-            }
-          ]
-        });
-        alert.present();
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public httpClient: HttpClient,
+    public loadingCtrl: LoadingController,
+    public geolocation: Geolocation
+  ) {
+    let loading1 = this.loadingCtrl.create({
+      content: 'Getting your location...'
+    });
+
+    loading1.present();
+
+    this.geolocation.getCurrentPosition().then((resp) => {
+      this.myLatitude = resp.coords.latitude;
+      this.myLongitude = resp.coords.longitude;
+      console.log("Got users location-->" + " " + this.myLatitude + " " + this.myLongitude);
+      loading1.dismiss();
+
+      let loading2 = this.loadingCtrl.create({
+        content: 'Closest location...'
       });
+
+      //request to api //
+      loading2.present();
+      this.closest = this.httpClient.get("http://krk-api.herokuapp.com/" + this.myLatitude + "," + this.myLongitude);
+      this.closest.subscribe(data => {
+        this.closestLocation = data["Closest location"]["name"]["eng"];
+        this.closestTour = data["Closest tour"]["name"]["eng"];
+        this.locationType = data["Closest location"]["type"];
+        this.locationImgSource = "assets/imgs/" + data["Closest location"]["key2"] + ".jpg";
+        this.tourImgSource = "assets/imgs/" + data["Closest tour"]["key2"] + ".jpg";
+        console.log(data["Closest location"]["name"]["eng"]);
+        loading2.dismiss();
+      });
+    }).catch((error) => {
+      console.log('Error getting users location', error);
+    });
+
   }
 
   refresh_page() {
